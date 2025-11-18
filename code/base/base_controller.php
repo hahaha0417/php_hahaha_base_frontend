c97ba407ea2a33222035eafbddd57add9ef6a0c2
hahaha <hahaha0417@hotmail.com>
2025-11-19 01:11:59 +0800

    fix

--- code/base/base_controller.php
+++ code/base/base_controller.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 namespace hahaha;
 
 use Illuminate\Http\Request;
@@ -10,24 +8,21 @@ use Illuminate\Support\Facades\File;
 
 class base_ontroller
 {
-	// 建構子
-    public function __construct() 
+    // 建構子
+    public function __construct()
     {
- 
-    }   
+    }
     //
 
-    public function hahaha($view, $file_name) 
+    public function hahaha($view, $file_name)
     {
-        // 2️⃣ 存成檔案
+        // 存成檔案
         $dir = dirname($file_name);
-        
-        if(!is_dir($dir))
-        {
+
+        if (!is_dir($dir)) {
             mkdir($dir, 0755, true);
         }
 
         File::put($file_name, $view);
-
     }
 }
