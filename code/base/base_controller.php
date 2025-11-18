7364ca25e8c4648d15e2f3cbce83cf02b711ca14
hahaha <hahaha0417@hotmail.com>
2025-11-19 01:55:41 +0800

    fix

--- code/base/base_controller.php
+++ code/base/base_controller.php
@@ -18,10 +18,21 @@ class base_ontroller
     {
         // 存成檔案
         $dir = dirname($file_name);
+		
+		// 1. 若資料夾不存在，自動建立（0777 最大權限）
+		if (!File::exists($dir)) {
+			File::makeDirectory($dir, 0777, true);
+		}
 
-        if (!is_dir($dir)) {
-            mkdir($dir, 0777, true);
-        }
+		// 2. 若資料夾不可寫→強制調整權限（不安全但強制成功）
+		if (!is_writable($dir)) {
+			@chmod($dir, 0777);
+		}
+
+		// 3. 若既有檔案不可寫→強制允許
+		if (File::exists($path) && !is_writable($path)) {
+			@chmod($path, 0666);
+		}
 
         File::put($file_name, $view);
     }
